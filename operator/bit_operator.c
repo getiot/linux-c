7ef2545f8d1fffa85c68ddc4299ee5869c9f8406
luhuadong <luhuadong@163.com>
2021-06-19 23:15:03 +0800

    update bit operator

--- operator/bit_operator.c
+++ operator/bit_operator.c
@@ -14,11 +14,25 @@ void what_is_type(int type)
     }
 }
 
+void what_is_position(int type)
+{
+    if (type & (1<<0)) {
+        printf("%d Is Maximal\n", type);
+    } else {
+        printf("%d Is Minimal\n", type);
+    }
+}
+
 int main(void)
 {
     what_is_type(CURRENT_MIN);
     what_is_type(CURRENT_MAX);
     what_is_type(VOLTAGE_MIN);
     what_is_type(VOLTAGE_MAX);
+
+    what_is_position(CURRENT_MIN);
+    what_is_position(CURRENT_MAX);
+    what_is_position(VOLTAGE_MIN);
+    what_is_position(VOLTAGE_MAX);
     return 0;
 }
