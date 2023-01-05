cb68eac96f8bf17b9b8ce24c8b0501746bce81ff
luhuadong <luhuadong@163.com>
2023-01-05 21:04:02 +0800

    [cpp] add main.cpp

---
+++ cpp/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 2;
+    int b = a + 3;
+
+    for (int i=0; i<10; i++) {
+        cout << "hello vs code & cmake..." << endl;
+    }
+
+    return 0;
+}
