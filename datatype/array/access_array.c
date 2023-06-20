441b1e9caa5835230034b4c8b5c2b146cfc50f32
luhuadong <luhuadong@163.com>
2023-06-20 17:36:01 +0800

    [array] update negative array index example

--- datatype/array/access_array.c
+++ datatype/array/access_array.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-#define INIT_SIZE 10   // 初始化数组大小
-#define INCREMENT 5    // 数组扩展大小
+static int32_t ARRAY_MIN = -3;
+static int32_t ARRAY_MAX = 10;
 
 int main()
 {
-    int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int a[ARRAY_MAX - ARRAY_MIN];
+    int *a_ = a - ARRAY_MIN;
 
-    for (int i=0; i<10; i++) {
+    /* Fill data */
+    for (int i=0; i<ARRAY_MAX-ARRAY_MIN; i++) {
+        a[i] = ARRAY_MIN + i;
+    }
+
+    /* Print data */
+    printf("Array: ");
+    for (int i=0; i<ARRAY_MAX-ARRAY_MIN; i++) {
         printf("%d ", a[i]);
     }
-    printf("\n");
+    printf("\n\n");
 
     printf("a[-1] = %d\n", a[-1]);
     printf("a[-2] = %d\n", a[-2]);
     printf("a[-3] = %d\n", a[-3]);
-    printf("a[10] = %d\n", a[10]);
+    printf("a[10] = %d\n\n", a[10]);
+
+    printf("a_[-1] = %d\n", a_[-1]);
+    printf("a_[-2] = %d\n", a_[-2]);
+    printf("a_[-3] = %d\n", a_[-3]);
+    printf("a_[10] = %d\n", a_[10]);
 
     return 0;
 }
